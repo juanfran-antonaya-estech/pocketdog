6df473ff24d6fdb193748500626fd895d5829806
juli <julian.gamez@escuelaestech.es>
2024-05-17 12:37:22 +0200

    bottombar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -47,7 +50,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation ("androidx.compose.material3:material3:1.2.1")
+    implementation ("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation ("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-beta01")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.4.2")
     implementation ("androidx.navigation:navigation-ui-ktx:2.4.2")
 
--- app/src/main/java/com/juanfra/pocketdog/ui/MainActivity.kt
+++ app/src/main/java/com/juanfra/pocketdog/ui/MainActivity.kt
@@ -6,16 +6,43 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.juanfra.pocketdog.R
+import com.juanfra.pocketdog.databinding.ActivityMainBinding
+import com.juanfra.pocketdog.databinding.FragmentInicioBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.bottomAppBar2.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
+
+        binding.bottomAppBar2.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.accelerator -> {
+                    // Handle accelerator icon press
+                    true
+                }
+                R.id.rotation -> {
+                    // Handle rotation icon press
+                    true
+                }
+                R.id.dashboard -> {
+                    // Handle dashboard icon press
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
