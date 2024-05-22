c0cb5ac4f467999b91c3780275f8896e9656a3c1
juli <julian.gamez@escuelaestech.es>
2024-05-22 11:39:18 +0200

    navegacion hecha

--- app/src/main/java/com/juanfra/pocketdog/ui/MainActivity.kt
+++ app/src/main/java/com/juanfra/pocketdog/ui/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.juanfra.pocketdog.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.juanfra.pocketdog.R
 import com.juanfra.pocketdog.databinding.ActivityMainBinding
 import com.juanfra.pocketdog.databinding.FragmentInicioBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHost
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -44,5 +58,11 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
--- app/src/main/java/com/juanfra/pocketdog/ui/fragment/InicioFragment.kt
+++ app/src/main/java/com/juanfra/pocketdog/ui/fragment/InicioFragment.kt
@@ -1,26 +1,38 @@
 package com.juanfra.pocketdog.ui.fragment
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.juanfra.pocketdog.R
+import com.juanfra.pocketdog.databinding.FragmentInicioBinding
 
 
 class InicioFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private var _binding: FragmentInicioBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+
+        _binding = FragmentInicioBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.barraInventario.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_misPerrosFragment)
+        }
+        binding.barraBatalla.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_misPerrosFragment)
+        }
+        binding.barraTienda.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_misPerrosFragment)
+        }
     }
 
 
