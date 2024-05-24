8714520e34d5219b3b503837fd00b4f6d67a469c
juli <julian.gamez@escuelaestech.es>
2024-05-24 11:12:00 +0200

    media player start

--- app/src/main/java/com/juanfra/pocketdog/ui/fragment/MisPerrosFragment.kt
+++ app/src/main/java/com/juanfra/pocketdog/ui/fragment/MisPerrosFragment.kt
@@ -1,5 +1,6 @@
 package com.juanfra.pocketdog.ui.fragment
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,15 +11,18 @@ import com.juanfra.pocketdog.R
 
 class MisPerrosFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mis_perros, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mediaPlayer = MediaPlayer.create(context, R.raw.pdtienda3)
+        mediaPlayer.start()
     }
 
 }
