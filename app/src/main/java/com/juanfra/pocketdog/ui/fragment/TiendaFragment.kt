f9931bdd4248806068f190830c0ebcbdde60901d
juli <julian.gamez@escuelaestech.es>
2024-05-24 09:51:48 +0200

    tiendaSergioimplementada

--- app/src/main/java/com/juanfra/pocketdog/ui/fragment/TiendaFragment.kt
+++ app/src/main/java/com/juanfra/pocketdog/ui/fragment/TiendaFragment.kt
@@ -5,56 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.juanfra.pocketdog.R
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.juanfra.pocketdog.data.doggos.Doggo
+import com.juanfra.pocketdog.databinding.FragmentTiendaBinding
+import com.juanfra.pocketdog.ui.adapter.TiendaAdapter
+import com.juanfra.pocketdog.ui.viewmodel.PesetasViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TiendaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TiendaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentTiendaBinding
+    private lateinit var adapter: TiendaAdapter
+    private val viewModel by activityViewModels<PesetasViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tienda, container, false)
+        binding = FragmentTiendaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TiendaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TiendaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAdapter()
+
+        var doggos : MutableLiveData<List<Doggo>> = MutableLiveData()
+        CoroutineScope(Dispatchers.IO).launch {
+            doggos.postValue(arrayListOf(viewModel.getRandomDoggo("comun"), (viewModel.getRandomDoggo("raro")), (viewModel.getRandomDoggo("epico")), (viewModel.getRandomDoggo("legendario"))))
+
+        }
+        doggos.observe(viewLifecycleOwner) {
+            adapter.updateList(ArrayList(it))
+        }
+
     }
+
+    fun setupAdapter() {
+        adapter = TiendaAdapter(ArrayList())
+        binding.rvTienda.adapter = adapter
+        binding.rvTienda.layoutManager = LinearLayoutManager(requireContext())
+    }
+
 }
